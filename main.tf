d0bae86998e499da404911b6496b62e72a2a49c3
Mitesh Gadaria <45635000+mitesh-gadaria@users.noreply.github.com>
2026-01-11 19:12:19 +0000

    Add Terraform and Azure provider configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 # Terraform main configuration will go here
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}

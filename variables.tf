cfc338ce016239f9b9c161dd9c1811b1d0621712
Mitesh Gadaria <45635000+mitesh-gadaria@users.noreply.github.com>
2026-01-11 19:13:20 +0000

    Add basic variables for project name and location
    Define variables for project name and location in Terraform.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
 # Terraform variables will be defined here
+
+variable "project_name" {
+  description = "Prefix used for naming Azure resources"
+  type        = string
+  default     = "terraform-azure-lab"
+}
+
+variable "location" {
+  description = "Azure region to deploy resources into"
+  type        = string
+  default     = "uksouth"
+}

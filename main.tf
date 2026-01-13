11b464fe6aa14d371f1fb8d20099a92adc83e5cb
Mitesh Gadaria <mitesh.gadaria@hotmail.co.uk>
2026-01-13 19:51:05 +0000

    Add Azure sotrage account and output name

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "~> 3.0"
     }
 
-random = {
+    random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
@@ -23,5 +23,24 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "${var.project_name}-rg"
   location = var.location
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "sa" {
+  # Storage account names must be globally unique, lowercase, and cannot contain hyphens
+  name                     = "${replace(var.project_name, "-", "")}${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  https_traffic_only_enabled = true
+
 }
 
